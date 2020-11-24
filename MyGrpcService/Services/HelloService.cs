028aab91bb3c1f2026a5ca9bce2f5788abd0176e
pawelkupka <pawel.kupka@gmail.com>
2020-11-24 21:28:45 +0100

    done

---
+++ MyGrpcService/Services/HelloService.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+
+
+namespace MyGrpcService
+{
+    public class HelloService : Hello.HelloBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
---
+++ MygRPCClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using MyGrpcService;
+
+namespace MygRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Hello.HelloClient(channel);
+            var response = await client.SayHelloAsync(new HelloRequest { Name = "Pawel" });
+            Console.WriteLine(response.Message);
+        }
+    }
+}
